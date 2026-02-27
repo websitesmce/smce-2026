1b1b180d7877c55f6de3ddfd62e727aba2dd4108
David-Pulidindi <benjuda19@gmail.com>
2026-02-27 13:27:54 +0530

    mandatory file name lowercase

--- src/components/navbar/Navigationbar.jsx
+++ src/components/navbar/Navigationbar.jsx
@@ -333,7 +333,7 @@ const linksData = [
       },
       {
         label: "Mandatory Disclosure",
-        href: "/public/pdfs/SMCE-MANDATORY-DISCLOSURE.pdf",
+        href: "/public/pdfs/smce-mandatory-disclosure.pdf",
         detail: "Mandatory Disclosure document as per AICTE norms.",
         external: true,
       },
